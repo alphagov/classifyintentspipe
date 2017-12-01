4608577d524b0023ba72b27b02a08bbdf065e22b
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-12-01 16:53:29 +0000

    Fix predictor in line with classifyintent v0.6.1

--- predictor.py
+++ predictor.py
@@ -1,17 +1,25 @@
 # coding: utf-8
+"""
+Make predictions on intent surveys using a pre-trained model
+"""
 
-print('********************************')
-print('***** Running predictor.py *****')
-print('********************************')
-
-from classifyintents import survey
+import pickle
+import sys
+import csv
+import logging
+import logging.config
+from datetime import datetime
 from os.path import basename, join, splitext
 import pandas as pd
 import numpy as np
-import pickle, sys
-from datetime import datetime
-import csv
 import scrubadub
+from classifyintents import survey
+
+# Setup logging
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('classifypipe')
+logger.info('Running predictor.py')
 
 # Handle command line arguments
 
@@ -25,13 +33,13 @@ def clean_if(x):
     scrubber.remove_detector('url')
     if isinstance(x, str):
         x = scrubber.clean(x)
-    return(x)
+    return x
 
 
 def main():
-    
+
     # Instantiate an instance of the survey class.
-    
+
     intent = survey()
 
     intent.load(input_file)
@@ -52,16 +60,19 @@ def main():
 
     # Remove obvious `none` cases where there is no free text.
 
-    no_comments = (intent.data['comment_further_comments'] == 'none') & (intent.data['comment_where_for_help'] == 'none') & (intent.data['comment_other_where_for_help'] == 'none') & (intent.data['comment_why_you_came'] == 'none')
-    
-    # Extract the respondent_ID of the easily classified `nones`. These will be
+    no_comments = ((intent.data['comment_further_comments'] == 'none') &
+                   (intent.data['comment_where_for_help'] == 'none') &
+                   (intent.data['comment_other_where_for_help'] == 'none') &
+                   (intent.data['comment_why_you_came'] == 'none'))
+
+    # Extract the respondent_id of the easily classified `nones`. These will be
     # used later for matching back in.
-    
-    easy_nones = intent.data.loc[no_comments,'respondent_ID'].astype(int)
-    
+
+    easy_nones = intent.data.loc[no_comments, 'respondent_id'].astype(int)
+
     # Exclude easily classifed nones from intent.data, so we don't try to classify
     # on these, but create a copy first for later matching.
-    
+
     intent.data_full = intent.data.copy()
     intent.data = intent.data.loc[~no_comments]
 
@@ -71,23 +82,25 @@ def main():
 
     # Import the saved model object from the training notebook
 
-    exported_pipeline = pickle.load(open(model,'rb'))
+    exported_pipeline = pickle.load(open(model, 'rb'))
 
     # Run the prediction
 
     predicted_classes = exported_pipeline.predict(intent.cleaned)
-    
-    print('Predicted classes:')
-    print(np.bincount(predicted_classes))
 
-    pd.Series(predicted_classes).to_csv('predicted_classes.csv')
-    
-    # Convert to a Series, name, and combine the respondent_ID with the predicted code. 
+    # Convert to a Series, name, and combine the respondent_id with the predicted code.
     # Then strip out those cases who are not coming out as OKs
 
-    predicted_classes = pd.Series(predicted_classes, index=intent.cleaned.index, name='ada_code')
-    predicted_classes = pd.concat([intent.data['respondent_ID'].astype('int'), predicted_classes], axis=1)
-    predicted_classes = predicted_classes.loc[predicted_classes['ada_code'] == 1,'respondent_ID']
+    predicted_classes = pd.Series(
+        predicted_classes,
+        index=intent.cleaned.index,
+        name='ada_code')
+
+    predicted_classes = pd.concat(
+        [intent.data['respondent_id'].astype('int'), predicted_classes],
+        axis=1)
+
+    predicted_classes = predicted_classes.loc[predicted_classes['ada_code'] == 1, 'respondent_id']
 
     # Combine the predicted OKs with the easily classified OKs.
 
@@ -95,62 +108,71 @@ def main():
 
     # Label the raw dataset with 'ok' and 'nones'
 
-    intent.raw['code'] = ''
-    intent.raw.loc[intent.raw['respondent_ID'].isin(predicted_classes),'code'] = 'ok'
-    intent.raw.loc[intent.raw['respondent_ID'].isin(easy_nones),'code'] = 'none'
-    
+    intent.raw['target'] = ''
+    intent.raw.loc[intent.raw['UserID'].isin(predicted_classes), 'target'] = 'ok'
+    intent.raw.loc[intent.raw['UserID'].isin(easy_nones), 'target'] = 'none'
+
+    print('Predicted classes:')
+    print(intent.raw['target'].value_counts())
+
     # Standardise the column type for respondent ID, for merging
-    
-    intent.raw['respondent_ID'] = intent.raw['respondent_ID'].astype('int')
-    intent.data['respondent_ID'] = intent.data['respondent_ID'].astype('int')   
-    
+
+    intent.raw['UserID'] = intent.raw['UserID'].astype('int')
+    intent.data['respondent_id'] = intent.data['respondent_id'].astype('int')
+
     # Run the pii_remover on all the free text fields
 
-    comment_cols = [i for i in intent.raw.columns if 'comment' in i] 
-    intent.raw.loc[:,comment_cols] = intent.raw.loc[:,comment_cols].applymap(clean_if)
+    comment_cols = [i for i in intent.raw.columns if 'comment' in i]
+    intent.raw.loc[:,comment_cols] = intent.raw.loc[:, comment_cols].applymap(clean_if)
 
 # Concatenate easy_nones with intent.data to ensure
     # Now merge the api lookup data into intent.raw
-        
-    urls = intent.data_full.loc[:,['respondent_ID','start_date','end_date','full_url','page','section','org']]
+
+    urls = intent.data_full.loc[:, ['respondent_id', 'start_date',
+        'end_date', 'full_url', 'page', 'section', 'org']]
 
     output = intent.raw.merge(
-            right=urls,
-            how='left',
-            left_on='respondent_ID',
-            right_on='respondent_ID'
-            )
-    
-        
+        right=urls,
+        how='left',
+        left_on='UserID',
+        right_on='respondent_id'
+        )
+
     # Remove the rather unhelpful US system dates, retaining only the clean ones.
-    
+
     # No longer need to drop US dates as these do not exist!
-    #output.drop(['respondent_ID','start_date','end_date','full_url'],axis=1,inplace=True)
+    #output.drop(['respondent_id','start_date','end_date','full_url'],axis=1,inplace=True)
 
     # Save the file out
-    
+
     output_file = join(
-            'output_data/classified',
-            splitext(basename(input_file))[0] + '_classified.csv'
-            )
+        'output_data/classified',
+        splitext(basename(input_file))[0] + '_classified.csv'
+    )
 
     url_file = join(
-            'output_data/',
-             splitext(basename(input_file))[0] + '_urls.csv'
-            )
+        'output_data/',
+        splitext(basename(input_file))[0] + '_urls.csv'
+        )
 
-    print('***** Saving predictions to ', output_file, ' *****')    
+    print('***** Saving predictions to ', output_file, ' *****')
 
-    output.to_csv(output_file)
+    output.to_csv(output_file, index=False)
 
-    print('***** Saving url lookups to ', url_file, ' *****')    
+    print('***** Saving url lookups to ', url_file, ' *****')
 
-    urls1 = urls.loc[:,['full_url' ,'page', 'section', 'org']]
+    urls1 = urls.loc[:, ['full_url', 'page', 'section', 'org']]
     urls1.drop_duplicates(inplace=True)
-    urls1.replace('^null$', np.nan, regex=True, inplace=True)
-    urls1['lookup_date'] = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())  
+
+    # Convert all to string for regex to work!
+
+    urls1 = urls1.applymap(str)
+    urls1.replace(to_replace='^null$', value=np.nan, regex=True, inplace=True)
+    urls1['lookup_date'] = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
+
+    # Use NONNUMERIC quotes to avoid issues with quotes in the csv itself
 
     urls1.to_csv(url_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
 
 if __name__ == '__main__':
-        main()
+    main()
